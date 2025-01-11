319d9065db461c7ff4d41468fa2032ec28314441
alex_ <alex_7377@hotmail.com>
2025-01-11 18:14:17 +0100

    age correctur

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
 
   return (
    <div>
-    <User firstname={name1} lastname='Petrov' age='29' username='sososo' email='mail1@gmail.com' />
-    <User firstname='Inga' lastname='Ivanova' age='13' username='alalala' email='mail2@gmail.com' />
-    <User firstname='Anna' lastname='Sidorova' age='33' username='hhnhnhn' email='mail3@gmail.com' />
-    <User firstname='Petr' lastname='Ushanov' age='55' username='rmrmrr' email='mail4@gmail.com' />
+    <User firstname={name1} lastname='Petrov' age={29} username='sososo' email='mail1@gmail.com' />
+    <User firstname='Inga' lastname='Ivanova' age={13} username='alalala' email='mail2@gmail.com' />
+    <User firstname='Anna' lastname='Sidorova' age={33} username='hhnhnhn' email='mail3@gmail.com' />
+    <User firstname='Petr' lastname='Ushanov' age={55} username='rmrmrr' email='mail4@gmail.com' />
    </div>
   )
 }
